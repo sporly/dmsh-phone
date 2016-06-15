06d4a70bf8db4b0e8d06f0298d35af18e92f64e7
sporly <326405926@qq.com>
2016-06-15 16:22:48 +0800

    补上链接

--- js/websrcipt.js
+++ js/websrcipt.js
@@ -14,6 +14,7 @@ new function() {
 		_self.changePage();
 	}, false);
 };
+
 /****回到顶部的设置* S***/
 $(function(){
 	var bt = $('#toolBackTop');
@@ -33,25 +34,31 @@ $(function(){
 			bt.hide();
 		}
 	});
+})
 	/****回到顶部的设置* E***/
 
 	/*收缩与展开的模块 S*/
-	$(".swrap-title").click(function() {
-		$(this).next('.d-brand').children('ul').slideToggle().parent().parent().siblings().children('.d-brand').children('ul').slideUp(500);
-		$(this).parent().siblings().removeClass('opened');
-		$(this).parent().toggleClass('opened');
-	});
+	$(function(){
+		$(".swrap-title").click(function() {
+			$(this).next('.d-brand').children('ul').slideToggle().parent().parent().siblings().children('.d-brand').children('ul').slideUp(500);
+			$(this).parent().siblings().removeClass('opened');
+			$(this).parent().toggleClass('opened');
+		});
+	})
 	/*收缩与展开的模块 E*/
 	  
 	/*单选  S*/
-	$(".radio-click>ul>li").click(function() {
-		$(this).children().addClass('d-checked');
-		$(this).siblings().children().removeClass('d-checked')
-	});
+	$(function(){
+		$(".radio-click>ul>li").click(function() {
+			$(this).children().addClass('d-checked');
+			$(this).siblings().children().removeClass('d-checked')
+		});
+	})
 	/*单选  E*/
 
 	/*升降排序 S*/
-	$(".sort-btn").on("click", function() {
+	$(function(){
+		$(".sort-btn").on("click", function() {
 			if (!$(this).children().hasClass("d-icon-rdown")) {
 				$(this).addClass("cur");
 				$(this).children().addClass("d-icon-rdown");
@@ -64,58 +71,69 @@ $(function(){
 			$(this).parent().siblings().children().children('.d-icon-down').removeClass('d-icon-rdown').removeClass('d-icon-up');
 			$(this).parent().siblings().children().removeClass('cur');
 		});
-
+   })
 	/*升降排序 E*/
 
 	/*tab切换的模块  S*/
-	var allbtn = $('.all-btn');
-	var clickbtn1 = $('.click-btn1');
-	var clickbtn2 = $('.click-btn2');
-	var clickbtn3 = $('.click-btn3');
-	var box = $('.phone-shopbox');
-	var box1 = $('.show-box1');
-	var box2 = $('.show-box2');
-	var box3 = $('.show-box3');
-	var menu = $('.my-order-menu a');
-	menu.click(function() {
-		menu.removeClass('cur');
-		$(this).addClass('cur');
-		$(this).children().children().remove();
-		menu.children('.pjsl').removeClass('cur');
-		$(this).children('.pjsl').addClass('cur');
-	});
-	clickbtn1.click(function() {
-		box.hide();
-		box1.show();
-	});
-	clickbtn2.click(function() {
-		box.hide();
-		box2.show();
-	});
-	clickbtn3.click(function() {
-		box.hide();
-		box3.show();
-	});
-	allbtn.click(function() {
-		box.show();
-	});
+	$(function(){
+		var allbtn = $('.all-btn');
+		var clickbtn1 = $('.click-btn1');
+		var clickbtn2 = $('.click-btn2');
+		var clickbtn3 = $('.click-btn3');
+		var box = $('.phone-shopbox');
+		var box1 = $('.show-box1');
+		var box2 = $('.show-box2');
+		var box3 = $('.show-box3');
+		var menu = $('.my-order-menu a');
+		menu.click(function() {
+			menu.removeClass('cur');
+			$(this).addClass('cur');
+			$(this).children().children().remove();
+			menu.children('.pjsl').removeClass('cur');
+			$(this).children('.pjsl').addClass('cur');
+		});
+		clickbtn1.click(function() {
+			box.hide();
+			box1.show();
+		});
+		clickbtn2.click(function() {
+			box.hide();
+			box2.show();
+		});
+		clickbtn3.click(function() {
+			box.hide();
+			box3.show();
+		});
+		allbtn.click(function() {
+			box.show();
+		});
+	})
 	/*tab切换的模块   E*/
     
     /*弹窗  S*/
-			var pbtn=$('.b-card-txt');
-			var bbtn=$('.dm-bomb-btn');
-			var popup=$('.dm-popup-box');
-			pbtn.click(function(){
-				popup.show();
-			});
-			bbtn.click(function(){
-				popup.hide();
-			});
+   $(function(){
+		var pbtn=$('.b-card-txt');
+		var bbtn=$('.dm-bomb-btn');
+		var popup=$('.dm-popup-box');
+		pbtn.click(function(){
+			popup.show();
+		});
+		bbtn.click(function(){
+			popup.hide();
+		});
+	})
     /*弹窗  E*/
-
+    	
+    /*菜单的出现于隐藏 S*/
+   $(function(){
+	   $('.d-mod-menu').click(function(){
+	   	    $('.d-mod-menu-list').toggle();
+	   });
+   })
+   /*菜单的出现于隐藏 E*/
 
 	/*固定在窗口顶部  S*/
-	var topm = $('.stick-tittle').offset().top;
+	/*var topm = $('.stick-tittle').offset().top;
 	$(window).scroll(function() {
 		if ($(window).scrollTop() >= topm) {
 			$(".stick-tittle").addClass("fixedtop");
@@ -131,11 +149,11 @@ $(function(){
 		} else {
 			$(".stick-tittle2").removeClass("fixedtop2");
 		}
-	});
+	});*/
 
 
 /*固定在窗口顶部  E*/
-});
+
 
 
 
